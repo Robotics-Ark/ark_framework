b545ad002e73ce4d63fe4355f2eff541e3139fcc
sarthakdas <sdas4@icloud.com>
2025-09-11 14:43:04 +0100

    logger node init

---
+++ ark/tools/data_logging/lcm_logger.py
@@ -0,0 +1,17 @@
+from ark.client.comm_infrastructure.base_node import BaseNode,  main
+from arktypes import string_t, flag_t
+from ark.tools.log import log
+from typing import Dict, Any, Optional
+import time
+import subprocess
+
+class LoggerNode(BaseNode):
+
+    def __init__(self, name: str, config: Optional[Dict[str, Any]] = None):
+        super().__init__(name)
+        self.pub = self.create_publisher("chatter", string_t)
+        self.create_service("logger/start", string_t, flag_t, self.start_logging)
+        self.create_service("logger/stop", flag_t, flag_t, self.stop_logging)
+
+if __name__ == "__main__":
+    main(LoggerNode)
