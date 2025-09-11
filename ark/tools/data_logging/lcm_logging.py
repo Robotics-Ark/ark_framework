7111d68a8e8696ceb8b3e2da58082a009be33d84
sarthakdas <sdas4@icloud.com>
2025-09-11 13:46:02 +0100

    start logging service

--- ark/tools/data_logging/lcm_logging.py
+++ ark/tools/data_logging/lcm_logging.py
@@ -13,6 +13,17 @@ class LoggerNode(BaseNode):
         self.create_service("logger/start", string_t, flag_t, self.start_logging)
         self.create_service("logger/stop", flag_t, flag_t, self.stop_logging)
 
+    def start_logging(self, channel: str, msg: string_t) -> flag_t:
+        log.info("Starting logging to file: ", msg.data)
+        self.proc = subprocess.Popen(
+            ['lcm-logger', msg.data],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            start_new_session=True,
+        )
+        msg = flag_t()
+        msg.flag = True
+        return msg
 
 
 if __name__ == "__main__":
