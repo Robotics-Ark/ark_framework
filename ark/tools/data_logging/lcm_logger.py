fdb10827c9c5a719207ead69e5874c74bb8e7f01
sarthakdas <sdas4@icloud.com>
2025-09-11 14:48:12 +0100

    typing changes

--- ark/tools/data_logging/lcm_logger.py
+++ ark/tools/data_logging/lcm_logger.py
@@ -1,13 +1,13 @@
 from ark.client.comm_infrastructure.base_node import BaseNode,  main
 from arktypes import string_t, flag_t
 from ark.tools.log import log
-from typing import Dict, Any, Optional
+from typing import Any, Optional
 import time
 import subprocess
 
 class LoggerNode(BaseNode):
 
-    def __init__(self, name: str, config: Optional[Dict[str, Any]] = None):
+    def __init__(self, name: str, config: Optional[dict[str, Any]] = None):
         super().__init__(name)
         self.create_service("logger/start", string_t, flag_t, self.start_logging)
         self.create_service("logger/stop", flag_t, flag_t, self.stop_logging)
