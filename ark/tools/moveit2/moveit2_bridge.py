ac696f9e2f4ed51ace04ccddd1ee8dc94f2b4fe2
Sarthak Das <sdas4@icloud.com>
2025-09-17 10:17:49 +0100

    Update moveit2_translator function type hints

--- ark/tools/moveit2/moveit2_bridge.py
+++ ark/tools/moveit2/moveit2_bridge.py
@@ -48,7 +48,7 @@ class MoveIt2Bridge(ArkRos2Bridge):
         # Init parent with the final mapping
         super().__init__(mapping_table=moveit2_mapping_table, global_config=global_config)
 
-    def moveit2_translator(self, ros_msg, ros_channel: str, ros_type: JointTrajectoryControllerState, ark_channel: str, ark_type: joint_group_command_t):
+    def moveit2_translator(self, ros_msg: JointTrajectoryControllerState, ros_channel: str, ros_type: type[JointTrajectoryControllerState], ark_channel: str, ark_type: type[joint_group_command_t]):
         """Convert joint state positions into Ark command."""
         msg = joint_group_command_t()
         msg.name = "arm"
