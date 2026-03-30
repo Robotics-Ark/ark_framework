1b5a7caae32e5f505b06939c3a470095d2229204
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2026-03-30 18:11:38 +0100

    Revamp node

--- src/ark/node.py
+++ src/ark/node.py
@@ -1,127 +1,103 @@
 import json
-import time
 import zenoh
-from ark.time.clock import Clock
-from ark.time.rate import Rate
-from ark.time.stepper import Stepper
-from ark.comm.publisher import Publisher
-from ark.comm.subscriber import Subscriber
-from ark.comm.querier import Querier
-from ark.comm.queriable import Queryable
-from ark.core.registerable import Registerable
+from collections.abc import Callable
+from google.protobuf.message import Message
+from ark.time import Clock, Rate, Stepper
+from ark.comm import Publisher, Subscriber, Querier, Queryable, Channel, Listener
 
 
-class BaseNode(Registerable):
+class Node:
 
-    def __init__(
-        self,
-        env_name: str,
-        node_name: str,
-        z_cfg: dict,
-        sim: bool = False,
-        collect_data: bool = False,
-    ):
-        self._z_cfg = zenoh.Config.from_json5(json.dumps(z_cfg))
-        self._session = zenoh.open(self._z_cfg)
-        self._env_name = env_name
+    def __init__(self, node_name: str, z_cfg: dict):
         self._node_name = node_name
-        self._data_collector = None
-        self._clock = Clock(self._session, sim, "clock")
-        self.core_registration()
+        self._session = self._init_zenoh_sesssion(z_cfg)
+
+        # Setup the clock
+        self.clock = Clock(self._sim, self._session)
+
+        # Setup publisher, subscriber, querier and queryable dictionaries
+        self._publishers = {}
+        self._subscribers = {}
+        self._queriers = {}
+        self._queryables = {}
+
+        # Setup dictionary to store rates and steppers
         self._rates = []
         self._steppers = []
-        self._pubs = {}
-        self._subs = {}
-        self._queriers = {}
-        self._queriables = {}
 
-        self._session.declare_subscriber(f"{env_name}/reset", self._on_reset)
+    def _init_zenoh_sesssion(self, z_cfg: dict):
+        _z_cfg = zenoh.Config.from_json5(json.dumps(z_cfg))
+        return zenoh.open(_z_cfg)
 
-    def _on_reset(self, sample: zenoh.Sample):
-        self.reset()
-
-    def reset(self):
-        pass  # can be overridden by subclasses if required
-
-    def core_registration(self):
-        print(".. todo: register node with ark core..")
-
-    def create_publisher(self, channel) -> Publisher:
+    def create_publisher(
+        self, channel: Channel, apply_noise: Callable[[Message], Message] | None = None
+    ) -> Publisher:
         pub = Publisher(
-            self._node_name,
-            self._session,
-            self._clock,
-            channel,
-            self._data_collector,
+            self._node_name, self._session, channel, self.clock, apply_noise=apply_noise
         )
-        pub.core_registration()
-        self._pubs[channel] = pub
+        self._publishers[channel] = pub
         return pub
 
-    def create_subscriber(self, channel, callback) -> Subscriber:
-        sub = Subscriber(
-            self._node_name,
-            self._session,
-            self._clock,
-            channel,
-            self._data_collector,
-            callback,
-        )
-        sub.core_registration()
-        self._subs[channel] = sub
+    def create_subscriber(
+        self, channel: Channel, callback: Callable[[Message], None]
+    ) -> Subscriber:
+        sub = Subscriber(self._node_name, self._session, channel, callback)
+        self._subscribers[channel] = sub
         return sub
 
-    def create_querier(self, channel, timeout=10.0) -> Querier:
+    def create_listener(
+        self, channel: Channel, n_buffer: int = 1, ready_when: str = "full"
+    ) -> Listener:
+        lr = Listener(self._node_name, self._session, channel, n_buffer, ready_when)
+        self._subscribers[channel] = lr
+        return lr
+
+    def create_querier(
+        self, channel: Channel, apply_noise: Callable[[Message], Message] | None = None
+    ) -> Querier:
         querier = Querier(
-            self._node_name,
-            self._session,
-            self._clock,
-            channel,
-            self._data_collector,
+            self._node_name, self._session, channel, self.clock, apply_noise=apply_noise
         )
-        querier.core_registration()
         self._queriers[channel] = querier
         return querier
 
-    def create_queryable(self, channel, handler) -> Queryable:
+    def create_queryable(
+        self,
+        channel: Channel,
+        callback,
+        apply_noise: Callable[[Message], Message] | None = None,
+    ) -> Queryable:
         queryable = Queryable(
             self._node_name,
             self._session,
-            self._clock,
+            self.clock,
             channel,
-            handler,
-            self._data_collector,
+            callback,
+            apply_noise=apply_noise,
         )
-        queryable.core_registration()
-        self._queriables[channel] = queryable
+        self._queryables[channel] = queryable
         return queryable
 
-    def create_rate(self, hz: float):
-        rate = Rate(self._clock, hz)
+    def create_rate(self, hz: float) -> Rate:
+        rate = Rate(self.clock, hz)
         self._rates.append(rate)
         return rate
 
     def create_stepper(self, hz: float, callback) -> Stepper:
-        stepper = Stepper(self._clock, hz, callback)
+        stepper = Stepper(self.clock, hz, callback)
         self._steppers.append(stepper)
+        stepper.start()
         return stepper
 
-    def spin(self):
-        while True:
-            time.sleep(1.0)
-
     def close(self):
-        closable_objs = (
-            self._steppers
-            + list(self._pubs.values())
-            + list(self._subs.values())
-            + list(self._queriers.values())
-            + list(self._queriables.values())
-        )
-        for obj in closable_objs:
-            obj.close()
-
+        for pub in self._publishers.values():
+            pub.close()
+        for sub in self._subscribers.values():
+            sub.close()
+        for querier in self._queriers.values():
+            querier.close()
+        for queryable in self._queryables.values():
+            queryable.close()
+        for stepper in self._steppers:
+            stepper.close()
         self._session.close()
-
-        if self._data_collector:
-            self._data_collector.close()
