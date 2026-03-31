dc2e581308b2a5d2c43672a6027807ab415399e5
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2026-03-31 18:39:55 +0100

    rename script filename, and change method name

--- src/ark/import_utils.py
+++ src/ark/import_utils.py
@@ -1,9 +1,7 @@
-import os
-from pathlib import Path
 from importlib import import_module
 
 
-def load_type(spec: str):
+def load_from_spec(spec: str):
     """
     Load and return an object from a string like:
 
@@ -48,26 +46,3 @@ def load_type(spec: str):
             ) from e
 
     return obj
-
-
-class CondaUtils:
-
-    @classmethod
-    def conda_root(cls) -> Path:
-        """Return the configured Conda root path."""
-        cr = os.environ.get("_CONDA_ROOT")
-        if cr is None:
-            raise EnvironmentError(
-                "Environment variable _CONDA_ROOT is not set. Cannot determine conda environments."
-            )
-        return Path(cr)
-
-    @classmethod
-    def get_python_executable(cls, env_name: str) -> str:
-        """Return the path to the Python executable for the given Conda environment."""
-        return str(cls.conda_root() / "envs" / env_name / "bin" / "python")
-
-    @classmethod
-    def get_active_conda_env(cls) -> str | None:
-        """Return the name of the currently active Conda environment. Returns None if no environment is active."""
-        return os.environ.get("CONDA_DEFAULT_ENV")
